83af9b9dbf8282b93096f9bfea4bad8ae30c0f8b
edvaldatli <edvaldatli@hotmail.com>
2017-10-26 19:05:40 +0000

    Linked templates to li elements

--- app/index/script/index.js
+++ app/index/script/index.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
   // Press submit button and enter to submit login
-  $(document).on('click keypress', function(e){
+  $(document).on('click keypress', function(e, callback){
     var target = $(e.target);
-    console.log(e.keyCode)
     if (target.is('#submit') || e.keyCode == 13) {
-      $.get('../templates/home.js', function(data){
-        var source = data;
-        var template = Handlebars.compile(source);
-        $('body').html(template);
-      });
+      // log in
+      init();
     }
   })
 })
+
+var liEventListeners = function(){
+  $('li').on('click', function(e){
+    var eAttr = e.target.id;
+    render(eAttr, '.rightside');
+  })
+}
+
+var render = function(filename, element, callback){
+  $.get('../templates/' + filename + '.js', function(data){
+    var source = data;
+    var template = Handlebars.compile(source);
+    $(element).html(template);
+  }).done(callback);
+}
+
+var init = function(){
+  render('init', 'body');
+  render('statistics', '.level');
+  render('sidebar', '.sidebar', liEventListeners);
+  render('home', '.rightside');
+}
